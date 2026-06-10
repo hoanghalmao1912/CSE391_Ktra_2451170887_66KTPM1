3b3f955b14e1f48f1e028a8b68dbe421a46f0d19
Nguyễn Hà <hanguyen@HoangHa.local>
2026-06-10 09:05:16 +0700

    Thêm thông tin README.md

--- EXAM2026_CaX/reactjs/src/App.jsx
+++ EXAM2026_CaX/reactjs/src/App.jsx
@@ -5,11 +5,15 @@ import heroImg from "./assets/hero.png";
 import "./App.css";
 import Header from "./components/Header";
 import ProductForm from "./components/ProductForm";
+import ProductTable from "./components/ProductTable";
+import ProductRow from "./components/ProductRow";
 function App() {
   return (
     <div className="App">
       <Header />
       <ProductForm />
+      <ProductTable />
+      <ProductRow />
     </div>
   );
 }
